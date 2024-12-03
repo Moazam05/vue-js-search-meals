8580f3ec9ecba6418f2a308a3901e9d515ca07c7
Moazam05 <salmanmoazam08@gmail.com>
2024-12-03 15:16:40 +0500

    MealsList

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 
 import Home from "../views/Home/index.vue";
+import MealsList from "../views/MealsList/index.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: Home,
+  },
+  {
+    path: "/letter/:letter",
+    name: "byLetter",
+    component: MealsList,
   },
 ];
 
---
+++ src/views/MealsList/index.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <div>Meals List</div>
+</template>
