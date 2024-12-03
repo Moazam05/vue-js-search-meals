f575308e48c19e1bbdd6af547855afcd73e921f5
Moazam05 <salmanmoazam08@gmail.com>
2024-12-03 16:21:37 +0500

    axios setup

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,8 +1,15 @@
 <script setup>
-import { computed } from "vue";
+import { computed, onMounted } from "vue";
 import store from "../../store";
+import axiosClient from "../../api/axiosClient.js";
 
 const meals = computed(() => store.state.meals);
+
+onMounted(() => {
+  axiosClient.get("list.php?c=list").then((response) => {
+    console.log("RRR", response.data);
+  });
+});
 
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 </script>
