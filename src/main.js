94438f0d36004b2713a683235a3423a6fff97c02
Moazam05 <salmanmoazam08@gmail.com>
2024-12-04 18:35:33 +0500

    vuetify setup

--- src/main.js
+++ src/main.js
@@ -3,6 +3,19 @@ import router from "./router";
 import store from "./store";
 import "./style.css";
 import "primeicons/primeicons.css";
+import "@mdi/font/css/materialdesignicons.css";
 import App from "./App.vue";
 
-createApp(App).use(router).use(store).mount("#app");
+import { createVuetify } from "vuetify";
+import "vuetify/styles"; // Import Vuetify's CSS styles
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+const app = createApp(App);
+
+app.use(router).use(store).use(vuetify).mount("#app");
