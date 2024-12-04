b50efc59c4ad0b52c76d97f60cb2d1bb5c25b806
Moazam05 <salmanmoazam08@gmail.com>
2024-12-04 18:16:09 +0500

    primeicons

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import router from "./router";
 import store from "./store";
 import "./style.css";
+import "primeicons/primeicons.css";
 import App from "./App.vue";
 
 createApp(App).use(router).use(store).mount("#app");
