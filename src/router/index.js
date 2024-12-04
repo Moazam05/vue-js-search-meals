e33b36e42386dcbe1c140402dd479ad4fb50c9dd
Moazam05 <salmanmoazam08@gmail.com>
2024-12-04 20:12:09 +0500

    center of the screen

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import Home from "../views/Home/index.vue";
 import MealsByLetter from "../views/MealsList/components/MealsByLetter.vue";
 import MealsByName from "../views/MealsList/components/MealsByName.vue";
 import MealDetails from "../views/MealsList/components/MealDetails.vue";
+import Login from "../views/Auth/Login.vue";
+import Signup from "../views/Auth/Signup.vue";
 
 const routes = [
   {
@@ -25,6 +27,16 @@ const routes = [
     path: "/meal/:id",
     name: "mealDetails",
     component: MealDetails,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/signup",
+    name: "Signup",
+    component: Signup,
   },
 ];
 
--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -31,46 +31,44 @@ const goToSignup = () => {
 </script>
 
 <template>
-  <v-container>
-    <v-row class="fill-height" align="center" justify="center">
-      <v-col cols="12" md="6">
-        <v-card>
-          <v-card-title>
-            <span class="headline">Login</span>
-          </v-card-title>
+  <div class="flex justify-center items-center h-[calc(100vh-100px)]">
+    <v-card class="w-[550px]">
+      <v-card-title class="mb-4">
+        <span class="headline">Login</span>
+      </v-card-title>
 
-          <v-card-text>
-            <v-form v-model="valid" ref="form" lazy-validation>
-              <v-text-field
-                v-model="email"
-                :rules="emailRules"
-                label="Email"
-                placeholder="Enter your email"
-                required
-              ></v-text-field>
+      <v-card-text>
+        <v-form v-model="valid" ref="form" lazy-validation>
+          <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="Email"
+            placeholder="Enter your email"
+            required
+            class="mb-2"
+          ></v-text-field>
 
-              <v-text-field
-                v-model="password"
-                :rules="passwordRules"
-                label="Password"
-                type="password"
-                placeholder="Enter your password"
-                required
-              ></v-text-field>
+          <v-text-field
+            v-model="password"
+            :rules="passwordRules"
+            label="Password"
+            type="password"
+            placeholder="Enter your password"
+            required
+            class="mb-2"
+          ></v-text-field>
 
-              <v-btn :disabled="!valid" @click="login" color="primary" block>
-                Login
-              </v-btn>
-            </v-form>
-          </v-card-text>
+          <v-btn :disabled="!valid" @click="login" color="primary mt-8" block>
+            Login
+          </v-btn>
+        </v-form>
+      </v-card-text>
 
-          <v-card-actions>
-            <v-btn text @click="goToSignup"
-              >Don't have an account? Sign Up</v-btn
-            >
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+      <v-card-actions class="justify-end">
+        <v-btn text class="text-capitalize" @click="goToSignup"
+          >Don't have an account? Sign Up</v-btn
+        >
+      </v-card-actions>
+    </v-card>
+  </div>
 </template>
